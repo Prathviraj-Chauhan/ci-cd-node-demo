6a54cf289e6eebaadce195677ee7d4972726d227
Prathviraj-Chauhan <prithvirajsingh746@gmail.com>
2026-05-03 14:21:04 +0530

    features updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Running Successfully');
+   res.send('Feature branch working ');
 });
 
 const PORT = 3000;
